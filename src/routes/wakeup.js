7292fbe649ea73cc87283d039e82c213dd65dcc9
Andrew-CC-Martin <amartin262@gmail.com>
2020-06-22 15:28:17 +1000

    fix wakeup endpoint

--- src/routes/wakeup.js
+++ src/routes/wakeup.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 //  endpoint used to wake up the free heroku server (which sleeps afer 15min of inactivity)
-router.get('/wakeup', (_req, res) => {
+router.get('/', (_req, res) => {
   res.sendStatus(200)
 })
 
